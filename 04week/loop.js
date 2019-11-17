ab0e2bac2d416c9fa3402e1250da88fdea6859a7
ajewe <ajewe@utexas.edu>
2019-11-17 14:19:45 -0600

    did numbers 1-3 on assignment

---
+++ 04week/loop.js
@@ -0,0 +1,30 @@
+
+let i = 1;
+
+ do {
+   console.log(i);
+   i++;
+ }
+ while (i <= 1000);
+
+ let person = {
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: 'Jan 5, 1925',
+  gender: "female"
+ };
+
+
+ let x;
+
+ for (x in person) {
+   if (x === 'birthDate') {
+    let birthSplit = person.birthDate.split(' ');
+    if (birthSplit[2]%2 === 0) {
+      return
+    } else {
+      return console.log(person.birthDate);
+    }
+   }
+  };
+ 
